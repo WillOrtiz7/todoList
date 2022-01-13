35ff7c9acb3b6a67c24529fa66167d9a815144fb
WillOrtiz7 <willortiz407@gmail.com>
2022-01-13 16:58:36 -0500

    Added filters as well as local storage

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event){
@@ -19,6 +22,8 @@ function addTodo(event){
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
+    // Add todo to local storage
+    saveLocalTodos(todoInput.value);
     // Checkmark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
@@ -43,6 +48,7 @@ function deleteCheck(event){
         const todo = item.parentElement;
         // Animation
         todo.classList.add('fall');
+        removeLocalTodos(todo);
         todo.addEventListener('transitionend', function(){
             todo.remove();
         });
@@ -53,4 +59,84 @@ function deleteCheck(event){
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+// Filters all, completed, and uncompleted items in the todo list
+function filterTodo(event){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(event.target.value){
+            case "all":
+                todo.style.display = 'flex';
+            break;
+            case "completed":
+            if (todo.classList.contains('completed')){
+                todo.style.display = 'flex';
+            }else{
+                todo.style.display = 'none';
+            }
+            break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = 'none';
+                }
+            break;
+        }
+    });
+}
+
+function saveLocalTodos(todo){
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos(){
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todo;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Checkmark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check-circle"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // Trash buttom
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    });
+}
+
+function removeLocalTodos(todo){
+    let todos;
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
